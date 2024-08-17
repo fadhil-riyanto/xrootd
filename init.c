9720461319aed7add98a68c6f7b497737173b64a
fadhil riyanto <me@fadev.org>
2024-08-17 10:25:47 +0700

    generate time
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- init.c
+++ init.c
@@ -1,4 +1,24 @@
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/epoll.h>
+
+#define debug(x) printf("%s\n", x)
+
+static char* get_time()
+{
+        time_t rawtime;
+        struct tm * timeinfo;
+
+        time (&rawtime);
+        timeinfo = localtime(&rawtime);
+
+        return asctime(timeinfo)
+}
+
 int main(void)
 {
+        get_time();
         return 0;
 }
