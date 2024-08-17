363fb0334bd853b8a1b784194b4ee6ae6d0145f7
fadhil riyanto <me@fadev.org>
2024-08-17 12:47:20 +0700

    add config.h
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- config.h
+++ config.h
@@ -1,1 +1,3 @@
-#define LITERATION_SLEEP 1              //
+#define LITERATION_SLEEP 1                                              // second
+#define BAT0_DIR         "/home/fadhil_riyanto/BALI64/xrootd/CMakeLists.txt"
+#define MAX_EVENTS      1
