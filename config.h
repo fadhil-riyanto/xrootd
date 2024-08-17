3e7869ca64f28cad50b237eb44df2424d1732488
fadhil riyanto <me@fadev.org>
2024-08-17 13:34:34 +0700

    updae bat0dir, add desktop name, and notify send path
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- config.h
+++ config.h
@@ -1,3 +1,5 @@
 #define LITERATION_SLEEP 1                                              // second
-#define BAT0_DIR         "/home/fadhil_riyanto/BALI64/xrootd/CMakeLists.txt"
+#define BAT0_DIR         "/sys/class/power_supply/BAT0/capacity"
 #define MAX_EVENTS      1
+#define DESKTOP_NAME     "FadhilLinux"
+#define NOTIFY_SEND_PATH     "/usr/bin/notify-send"
