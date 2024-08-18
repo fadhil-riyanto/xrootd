e1de4a440fdbd59fcb168b6e75b150d2986ffa9d
fadhil riyanto <me@fadev.org>
2024-08-18 10:46:34 +0700

    add charging state file
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- config.h
+++ config.h
@@ -1,5 +1,6 @@
 #define LITERATION_SLEEP 1                                              // second
 #define BAT0_DIR         "/sys/class/power_supply/BAT0/capacity"
+#define BAT_CHARGING_STATE_0_DIR         "/sys/class/power_supply/BAT0/status"
 #define MAX_EVENTS      1
 #define DESKTOP_NAME     "FadhilLinux"
 #define NOTIFY_SEND_PATH     "/usr/bin/notify-send"
